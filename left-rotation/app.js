59a0a8689905e3d28b8b1ec4621013ce779eaedc
Guillermo García <guillermieres97@gmail.com>
2022-11-19 00:32:23 +0100

    new array exercise left rotation

--- left-rotation/app.js
+++ left-rotation/app.js
@@ -1,9 +1,26 @@
-"use strict"
+"use strict";
 
 const nums = [1, 2, 3, 4];
 const rotations = 2;
 
 function rotateLeft(d, arr) {
-    // Write your code here
+  const newArr = [];
 
+  const numRotations = d - 1;
+
+  for(let i = 0; i <= numRotations; i++) {
+    const index = 0;
+
+    newArr.push(arr[index]);
+
+    arr.splice(index, 1);
+  }
+
+  newArr.forEach((el) => {
+    arr.push(el);
+  });
+
+  return arr;
 }
+
+console.log(rotateLeft(rotations, nums));
